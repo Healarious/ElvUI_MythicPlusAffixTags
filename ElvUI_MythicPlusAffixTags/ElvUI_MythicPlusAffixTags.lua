97b2bccd3b9a39f5ff69627b12d57f187fd820fb
Healz <healarious.eu.ravencrest@gmail.com>
2017-07-19 11:03:12 +0200

    Some code updates

--- ElvUI_MythicPlusAffixTags/ElvUI_MythicPlusAffixTags.lua
+++ ElvUI_MythicPlusAffixTags/ElvUI_MythicPlusAffixTags.lua
@@ -3,51 +3,41 @@ local ElvUF = ElvUI.oUF
 ElvUF.Tags.OnUpdateThrottle["affix:necrotic-rot"] = 0.2
 ElvUF.Tags.Methods["affix:necrotic-rot"] = function(unit)
 	local rot = select(4, UnitDebuff(unit, GetSpellInfo(209858)))
-	local str = ""
 	if rot then
-		str = rot
+		return WrapTextInColorCode(rot, "ffbfff00")
 	end
-	return WrapTextInColorCode(str, "ffbfff00").."|r"
 end
 
 ElvUF.Tags.OnUpdateThrottle["affix:necrotic-rot-percent"] = 0.2
 ElvUF.Tags.Methods["affix:necrotic-rot-percent"] = function(unit)
 	local rot = select(4, UnitDebuff(unit, GetSpellInfo(209858)))
-	local str = ""
 	if rot then
-		str = (rot*3).. "%"
+		return WrapTextInColorCode((rot*3).. "%", "ffbfff00")
 	end
-	return WrapTextInColorCode(str, "ffbfff00").."|r"
 end
 
 ElvUF.Tags.OnUpdateThrottle["affix:bursting"] = 0.2
 ElvUF.Tags.Methods["affix:bursting"] = function(unit)
 	local bursting = select(4, UnitDebuff(unit, GetSpellInfo(240443)))
-	local str = ""
 	if bursting then
-		str = bursting
+		return WrapTextInColorCode(bursting, "ffff4500")
 	end
-	return WrapTextInColorCode(str, "ffff4500").."|r"
 end
 
 ElvUF.Tags.OnUpdateThrottle["affix:bursting-percent"] = 0.2
 ElvUF.Tags.Methods["affix:bursting-percent"] = function(unit)
 	local bursting = select(4, UnitDebuff(unit, GetSpellInfo(240443)))
-	local str = ""
 	if bursting then
-		str = (bursting*2.5).. "%/s"
+		WrapTextInColorCode((bursting*2.5).. "%/s", "ffff4500")
 	end
-	return WrapTextInColorCode(str, "ffff4500").."|r"
 end
 
 ElvUF.Tags.OnUpdateThrottle["affix:grievous"] = 0.2
 ElvUF.Tags.Methods["affix:grievous"] = function(unit)
 	local grievous = select(4, UnitDebuff(unit, GetSpellInfo(240559)))
-	local str = ""
 	if grievous then
-		str = grievous
+		WrapTextInColorCode(grievous, "ffff0000")
 	end
-	return WrapTextInColorCode(str, "ffff0000").."|r"
 end
 
 ElvUF.Tags.OnUpdateThrottle["affix:grievous-percent"] = 0.2
@@ -63,7 +53,7 @@ ElvUF.Tags.Methods["affix:grievous-percent"] = function(unit)
 	elseif grievous == 4 then
 		str = "10 %"
 	elseif grievous == 5 then
-		return "12 %"
+		str = "12 %"
 	end
-	return WrapTextInColorCode(str, "ffff0000").."|r"
+	return WrapTextInColorCode(str, "ffff0000")
 end
